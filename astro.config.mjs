63ead28b9dafce20d45af57d8932e0de893feab2
ramiro <ramicurros@gmail.com>
2026-03-06 12:20:50 -0300

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ramicurros.github.io/agentlist-mock/',
-    base: '/'
+    site: 'https://ramicurros.github.io/agentlist-mockup/',
+    integrations: [mdx(), sitemap(), react()],
+    build: {
+        inlineStylesheets: 'always', // Set to always inline stylesheets
+    },
+    base: '/agentlist-mockup/',
+    devToolbar: {
+        enabled: false
+    }
 });
